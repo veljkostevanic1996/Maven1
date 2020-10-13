0b4676f8a74e2d1b94e2533988e2b0435f3da9f1
veljkostevanic1996 <stevanicveljko1996@gmail.com>
2020-10-13 01:52:01 -0700

    change something

---
+++ src/main/java/SpockTest.groovy
@@ -0,0 +1,18 @@
+/*import spock.lang.Specification
+
+class SpockTest extends Specification{
+
+    int sum
+
+    def setup(){
+        sum = 2
+        println("suma je incijalizovana na $sum")
+    }
+
+    def 'one plue one should equal two'(){
+
+        expect:
+        1 + 1 == sum
+    }
+}
+*/
